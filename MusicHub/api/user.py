e9a8e9d2882b43f252e6a6d0b5de361a75806ed8
Damian Bednarek <Damian.Bednarek@itechart-group.com>
2022-12-16 09:04:33 +0100

    implemented login functionality

--- MusicHub/api/user.py
+++ MusicHub/api/user.py
@@ -1,13 +1,43 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from MusicHub.schemas.userSchema import CreateUser, BaseUser
+from MusicHub.schemas import userSchema, tokenSchema
+from MusicHub.core.security import create_token
 from MusicHub.api.depends import get_db
 from MusicHub.crud import userCrud
 
 router = APIRouter()
 
 
-@router.post("/create-user", response_model=BaseUser)
-def create_user(user: CreateUser, db: Session = Depends(get_db)):
+@router.post("/create-user", response_model=userSchema.BaseUser)
+def create_user(user: userSchema.CreateUser, db: Session = Depends(get_db)):
     user = userCrud.create_user(user, db)
-    return user
+    if user:
+        return user
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email address is already in use",
+        )
+
+
+@router.post("/login", response_model=tokenSchema.Token)
+def login(
+    db: Session = Depends(get_db), credentials: OAuth2PasswordRequestForm = Depends()
+):
+    user = userCrud.autenticate_user(credentials.username, credentials.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid login or password",
+        )
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User is not active",
+        )
+
+    return {
+        "access_token": create_token({"sub": user.email}),
+        "token_type": "Bearer",
+    }
--- MusicHub/crud/userCrud.py
+++ MusicHub/crud/userCrud.py
@@ -1,15 +1,32 @@
 from MusicHub.schemas.userSchema import CreateUser
 from MusicHub.models.user import User
 from sqlalchemy.orm import Session
-from MusicHub.core.security import get_password_hash
+from MusicHub.core.security import get_password_hash, check_passwords_match
+from psycopg2.errors import UniqueViolation
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
 
 
 def create_user(user: CreateUser, db: Session):
     user_dict = user.dict()
     user_dict.pop("confirm_password")
     user_dict["password"] = get_password_hash(user_dict["password"])
+    if get_user_by_email(db, user_dict["email"]):
+        return None
     db_user = User(**user_dict)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
+
     return db_user
+
+
+def autenticate_user(email: str, password: str, db: Session):
+    user = get_user_by_email(db, email)
+    if not user:
+        return None
+    if not check_passwords_match(password, user.password):
+        return None
+    return user
